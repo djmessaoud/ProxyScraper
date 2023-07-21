d162fa7f27aa3d33eb3478e20eb8f183f6daa50f
Djardi Messaoud <djardi.messaoud@gmail.com>
2023-07-21 19:24:58 +0100

    Added project files
    Added project files

---
+++ ProxyScrap/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace ProxyScrap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.proxyBox = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linksBOx = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(381, 25);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(15, 16);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(76, 43);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // proxyBox
+            // 
+            this.proxyBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.proxyBox.Location = new System.Drawing.Point(471, 2);
+            this.proxyBox.Margin = new System.Windows.Forms.Padding(2);
+            this.proxyBox.Name = "proxyBox";
+            this.proxyBox.Size = new System.Drawing.Size(328, 387);
+            this.proxyBox.TabIndex = 1;
+            this.proxyBox.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 391);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(823, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(40, 17);
+            this.toolStripStatusLabel1.Text = "Lines :";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.RightToLeftAutoMirrorImage = true;
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel2.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(385, 110);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Boom Boom";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linksBOx
+            // 
+            this.linksBOx.Location = new System.Drawing.Point(0, -1);
+            this.linksBOx.Margin = new System.Windows.Forms.Padding(2);
+            this.linksBOx.Name = "linksBOx";
+            this.linksBOx.Size = new System.Drawing.Size(378, 390);
+            this.linksBOx.TabIndex = 4;
+            this.linksBOx.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 413);
+            this.Controls.Add(this.linksBOx);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.proxyBox);
+            this.Controls.Add(this.webBrowser1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.RichTextBox proxyBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox linksBOx;
+    }
+}
+
---
+++ ProxyScrap/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+namespace ProxyScrap
+{
+    public partial class Form1 : Form
+    {
+        string pattern = @"(\d{1,3}\.){3}\d{1,3}:\d{2,5}";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Regex reg = new Regex(pattern);
+            for (int i=0;i<linksBOx.Lines.Length;i++)
+            {
+                if (reg.IsMatch(linksBOx.Lines[i])) proxyBox.Text += linksBOx.Lines[i] + "\n";
+
+            }
+           /* MatchCollection matches = reg.Matches(p);
+            for (int i = 0; i < matches.Count; i++)
+            {
+                MessageBox.Show(matches[i].ToString());
+                proxyBox.Text += matches[i].ToString() + "\n";
+            }*/
+           /* WebClient client = new WebClient();
+            int nProxy  = proxyBox.Lines.Length;
+            for (int i=0;i<nProxy;i++)
+            {
+                string page = client.DownloadString(proxyBox.Lines[i]);
+
+            }*/
+            
+        }
+    }
+}
